285b4d0fc28636e90a07ec90f24c592f561f0e04
Kakonoomoide <rizkiramada128@gmail.com>
2024-12-30 01:38:08 +0700

    UPDATE - Greeting menjadi MahasiswaApp pada tampilan

--- app/src/main/java/com/example/pam_pert10/MainActivity.kt
+++ app/src/main/java/com/example/pam_pert10/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pam_pert10.ui.theme.PAMPert10Theme
+import com.example.pam_pert10.ui.viewmodel.MahasiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PAMPert10Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MahasiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
